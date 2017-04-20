836affaedf97f178634b96e94cb6eb162da44ba5
Yongyao Jiang <J.yongyao11@gmail.com>
2017-04-19 23:45:01 -0400

    First experiment (good)

--- makemodel.py
+++ makemodel.py
@@ -7,7 +7,7 @@ Created on Wed Apr 19 16:29:38 2017
 """
 
 import gensim, logging, nltk, re, os, sys
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.snowball import SnowballStemmer
 from gensim.models import Phrases
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -17,7 +17,7 @@ sys.setdefaultencoding('utf-8')
 #==============================================================================
 
 stopwords = nltk.corpus.stopwords.words('english')
-stemmer = LancasterStemmer()
+stemmer = SnowballStemmer("english")
 
 def tokenize_stem_stop(text):
     # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
@@ -27,16 +27,17 @@ def tokenize_stem_stop(text):
     for token in tokens:
         if re.search('[a-zA-Z0-9]', token):
             filtered_tokens.append(token)
-    #stems = [stemmer.stem(t) for t in tokens]
-    return filtered_tokens
+    stems = [stemmer.stem(t) for t in tokens]
+    return stems
 
 def makesent(path):
+    result = []
     for line in open(path):
-        out = tokenize_stem_stop(line.lower())
-        yield out
+        result.append(tokenize_stem_stop(line.rsplit(' ', 1)[0].lower()))
+    return result
 
 sentences = makesent('/Users/yjiang/Documents/pythonWorkspace/freqCounter/agg_self.txt')
-print(list(sentences))
+print(sentences)
 bigram = Phrases(sentences, min_count=1, threshold=1)
 bi_sent = bigram[sentences]
 trigram = Phrases(bigram[sentences], min_count=1, threshold=1)
--- recalculate.py
+++ recalculate.py
@@ -7,11 +7,52 @@ Created on Wed Apr 19 16:40:46 2017
 """
 
 import gensim, logging, nltk, re, os, sys
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.snowball import SnowballStemmer
 from gensim.models import Phrases
+
+stopwords = nltk.corpus.stopwords.words('english')
+stemmer = SnowballStemmer("english")
 
 bigram = Phrases.load('model/bigram')
 trigram = Phrases.load('model/trigram')
-sent = ['near', 'earth', 'object']
 
-print(trigram[bigram[sent]])
+def tokenize_stem_stop(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for word in nltk.word_tokenize(text) if word not in stopwords]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z0-9]', token):
+            filtered_tokens.append(token)
+    #stems = [stemmer.stem(t) for t in tokens]
+    return filtered_tokens
+
+def recal(path):
+    result = {}
+    for line in open(path):
+        text = tokenize_stem_stop(line.rsplit(' ', 1)[0].lower())
+        parsed_text = trigram[bigram[text]]
+        value = int(line.rsplit(' ', 1)[1])
+        for term in parsed_text:
+            w = 1
+            if not term.isdigit() and len(term)>2:
+                if len(text)>4 and '_' in term:
+                   w = 50
+                if term in result:
+                   result[term] = result[term] + value*w
+                else:
+                   result[term] = value*w
+    return result
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+output = recal('/Users/yjiang/Documents/pythonWorkspace/freqCounter/agg_self.txt')
+sorted_output = sorted(output.items(), key=lambda x: x[1], reverse=True)
+with open("output.txt", "w") as text_file:
+    for out in sorted_output:
+        if not hasNumbers(out[0]) and out[1]>10:
+               text_file.write("{0} {1}\n".format(out[0], out[1]))
+
+# sent = ['near', 'earth', 'object', 'planetary', 'society']
+# print(trigram[bigram[sent]])
